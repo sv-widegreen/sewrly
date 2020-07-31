a132ed061f3cff53d1c60884696618d6a77b47a8
sv-widegreen <svenja@wide.green>
2020-07-31 16:58:35 +0200

    Add stories

---
+++ src/components/FabricCalculatorDropdown.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import FabricCalculatorDropdown from './FabricCalculatorDropdown'
+
+export default {
+  title: 'Fabric Calculator Dropdown',
+  component: FabricCalculatorDropdown,
+}
+
+export const fabricCalculatorDropdown = () => <FabricCalculatorDropdown />
